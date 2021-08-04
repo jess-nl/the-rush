c354c35454e2205f2a167c5cccd9edd855332070
Jess Noriega-Lessard <jess@MacBook-Pro-2.local>
2021-08-04 19:30:25 -0400

    ping remote api with express and axios

--- index.ts
+++ index.ts
@@ -9,3 +9,17 @@ require("dotenv/config");
 router.listen(port, () => {
   console.log(`Listening at ${port}`);
 });
+
+router.get(`/api/v1/therush`, async (req, res) => {
+  try {
+    await axios.get(process.env.THERUSH_URL as string).then((resp) => {
+      const { data } = resp;
+
+      res.send(data);
+      return;
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(e.status || 500).send({ ...e, message: e?.message });
+  }
+});
